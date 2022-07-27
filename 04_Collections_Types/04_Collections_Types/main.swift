c8b8b668aae2531a9210101eda6ad9209a8e1c1a
Dylan-Yoon <d.dylany21@gmail.com>
2022-07-27 19:17:54 +0900

    04 배열 정리

--- 04_Collections_Types/04_Collections_Types/main.swift
+++ 04_Collections_Types/04_Collections_Types/main.swift
@@ -31,16 +31,49 @@ func arrays() {
     let creatingArray5 = Array(repeating: ["2차원", "배열", "생성"], count: 10)
     print(creatingArray5)
     
+    //배열의 접근과 수정 (Accessing and Modifying an Array)
+    //메서드 들을 사용해 접근 및 수정 가능하다.
+    var methodArrays = [1,2,3,4,6,7,8,9]
+    print(methodArrays.isEmpty) //false
+    print(methodArrays.count) //10 Int
+    
+    methodArrays.append(10)  //맨 마지막에 추가
+    print(methodArrays)
+    
+    methodArrays += [11]
+    print(methodArrays)
+    
+    methodArrays.insert(5, at: 4)
+    print(methodArrays)
+    
+    methodArrays.remove(at: 1)
+    print(methodArrays)
+    
+    //CAUTION : 배열의 범위를 넘는 인덱스로 값에 접근 또는 수정을 할 경우 런타임 에러가 발생.
+    methodArrays.removeLast()
+    print(methodArrays)
+
+    
+    //배열은 반복문에 넣어 사용 할 수 있다.
+    // (Iterating Over an Array)
+    
+    for i in methodArrays {
+        print(i)
+    }
+    
+    
+    //배열을 값 뿐만 아니라 인덱스를 사용하기 위해서 enumerated() 를 사용하면 된다.
+    for (index, value) in methodArrays.enumerated() {
+        print("Item \(index + 1): \(value)")
+    }
+    
+    for (index, _) in methodArrays.enumerated() {
+        print("index: ", index)
+    }
+    
 }
 
-arrays()
-
-
-
-
-
-
-
+//arrays()
 
 //MARK: - 집합(Sets)
 
