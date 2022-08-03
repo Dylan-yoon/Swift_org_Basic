e18ac830ade3ce683c4f385350ec6eb7c0d9ca85
dylan-yoon <d.dylany21@gmail.com>
2022-08-03 14:05:34 +0900

    04 Set, Dictionary

--- 04_Collections_Types/04_Collections_Types/main.swift
+++ 04_Collections_Types/04_Collections_Types/main.swift
@@ -86,25 +86,65 @@ func arrays() {
  */
 
 func sets() {
-    var sets1: Set<String> = ["aaaa","bbbb","cccc"] // [배열 형식을 타입 지정으로 세트로 지정 가능]
+    var sets1: Set<String> = ["a","b","c"] // [배열 형식을 타입 지정으로 세트로 지정 가능]
     
     for i in sets1 {
         print(i)
     } //Set도 반복문에 사용 가능
-}
-
-// 실행
- sets()
-
-
-// set 오ㅓ
-
-//MARK: - 딕셔너리(Dicitionaries)
-
-func dictionaries() {
+    
+    
+    
+    // 집합을 사용 할 수 있음.
+    /*
+     Set 집합
+     intersection 교집합
+     symmetricDifference 대칭차집합
+     union 합집합
+     subtracting 여집합
+     
+     a.somethingSet(b) 으로 사용한다.
+     
+     
+     
+     isSubset(of:)
+     isSuperset(of:)
+     isStrictSuperset(of:)
+     isStrictSubset(of:)
+     isDisjoint(with:)
+     */
+    
     
 }
 
+// 실행
+// sets()
+
+
+//MARK: - 딕셔너리(Dicitionaries)
+//key- value 의 쌍우로 조합된다.
+//Dictionary<Key, Value>
+func dictionaries() {
+    var dic: Dictionary<String, String> = [String: String]()
+    dic["a"] = "AA"
+    dic["b"] = "BB"
+    dic["c"] = "AA" //값은 같을 수 있다.
+    dic["a"] = "BB" //키의 값 변경
+    print(dic)
+    
+    
+    for i in dic {
+        print(i)
+    }
+    //실행 시 계속해서 순서가 바뀌는 것을 알 수 있다.
+    
+    // dictionary 는 For - in 루프에서 사용 할 때 (key,value) 튜플의 한 쌍으로 사용된다.
+    for (a, b) in dic {
+        print("\(a) and \(b)")
+    }
+    // dictionary의 key 혹은 value만을 사용하기 위해서는 dic.keys 혹은 dic.values를 사용한다.
+}
+
+dictionaries()
 
 
 
