985ac3afdb78b38b588da8e6217506369c45da0f
dylan-yoon <d.dylany21@gmail.com>
2022-08-06 01:28:49 +0900

    05 while

--- 05_Control_Flow/05_Control_Flow/main.swift
+++ 05_Control_Flow/05_Control_Flow/main.swift
@@ -51,11 +51,64 @@ func forIn() {
  */
 
 
+/*
+ 
+ Repeat-While
+ repeat {
+    (statements)
+ } while (condition)
+ */
+
+func exWhile() {
+    var a = 0
+    print("while")
+    while a < 3 {
+        print("a : \(a)")
+        a += 1
+    }
+    
+    //repeat while을 사용하면
+    a = 0
+    print("repeat while")
+    repeat {
+        print("a : \(a)")
+        a += 1
+    } while a < 3
+    
+    //여기까지는 똑같다 하지만
+    print("----0----0----0---0---0---0---0----")
+    var b = 3
+    print("while")
+    while b < 3 {
+        print("b : \(b)")
+        a += 1
+    }
+    
+    //repeat while을 사용하면
+    b = 3
+    print("repeat while")
+    repeat {
+        print("a : \(b)")
+        b += 1
+    } while b < 3
+    
+    /*
+     - while과 repeat-while을 사용 시 유의사항 -
+     조건이 바로 일치 되더라도 while 최초 1번을 사용하기 위해서 사용한다.
+
+     위에서 처럼 실행되지 않는다는 (Will never be executed) 경고창이 나타난다
+     */
+}
+
+//exWhile()
+
+//MARK: - If
 
 
 
-
-//MARK: -
-
-
+/*
+ - 배열과 반복문에서의 주의할점
+ 배열을 반복문 혹은 인덱스를 사용 할 때
+ 배열의 범위를 넘어가지 않도록 주의 -> 런타임 오류 발생
+ */
 
