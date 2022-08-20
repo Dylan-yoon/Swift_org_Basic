da036e66e566d55c3431273ef43e269226f3502a
dylan-yoon <d.dylany21@gmail.com>
2022-08-20 12:39:26 +0900

    10 properties 임시

--- 10_Properties/10_Properties/main.swift
+++ 10_Properties/10_Properties/main.swift
@@ -5,7 +5,45 @@
 //  Created by Dylan_Yoon on 2022/06/20.
 //
 
-import Foundation
+//MARK: - Stored Properties
+// 1. variable stored properties (keyWord 'var')
+// 2. constat stored properties (keyWord 'let')
 
-print("Hello, World!")
+
+//MARK: Stored Properties of Constant Structure Instances
+// * 구조체의 인스턴스를 생성하고 상수로 할당하면 프로퍼티 변수로 선언되어 있어도 인스턴스의 프로퍼틸르 수정 할 수 없다.
+
+//MARK: Lazy Stored Properties
+// 처음 사용될 때까지 초기값은 계산되지 않는 프로퍼티.
+//
+
+/*lazy*/ var lazyStoredProperty = 1 //'lazy' cannot be used on an already-lazy global
+
+struct LazyStored {
+//    상수로는 lazy 선언 불가 => 
+//    lazy let constantLazyStoredProperty = 1
+    lazy var lazyStoredProperty = 1
+}
+
+
+
+
+//MARK: -
+
+
+
+//MARK: -
+
+
+
+
+//MARK: -
+
+
+//MARK: -
+
+//MARK: -
+
+
+
 
