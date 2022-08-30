2ee9d9651ebbb61e03e43badeaf132617df91d31
dylan-yoon <d.dylany21@gmail.com>
2022-08-31 03:09:37 +0900

    11 mutating method and Assigning self

--- 11_Methods/11_Methods/main.swift
+++ 11_Methods/11_Methods/main.swift
@@ -84,3 +84,75 @@ let subClassInstance = SubClassInstance()
 subClassInstance.오른쪽으로()
 subClassInstance.위치조정(x: 3, y: 10)
 
+
+print("------------------------------------------------")
+//MARK: -Modifying Value Types from Within Instance Method (인스턴스 메서드 내에서 값 타입 수정), mutating
+print("Modifying Value Types from Within Instance Method")
+// 값 타입의 프로퍼티는 인스턴스 메서드 내에서 수정될 수 없다. Struct, Enum
+
+//mutating
+
+//하지만 mutating 키워드를 사용해서 프로퍼티의 수정이 가능하다
+// Left side of mutating operator isn't mutable: 'self' is immutable
+// mutating 사용하지 않는다면 위와 같은 오류가 난다.
+
+struct UsingMutating {
+    var name = "struct"
+    
+    mutating func changeName() {
+        name += "mutating" // <- name 암시적 self 프로퍼티에 새로운 인스턴스를 할당.
+        print("print name : ", name)
+    }
+}
+
+var usingMutating = UsingMutating()
+usingMutating.changeName()
+
+
+print("------------------------------------------------")
+//MARK: -변경 메서드 내에서 self 할당 (Assigning to self Within a Mutating Method)
+print("Assigning to self Within a Mutating Method")
+
+
+struct UsingMutatingAndAssigningSelf {
+    var name = "struct"
+    
+    mutating func changeName() {
+        name = "ssssssssssssssssss"
+        self = UsingMutatingAndAssigningSelf(name: "string")
+        print("print name : ", name)
+    }
+}
+var usingMutatingAndAssigningSelf = UsingMutatingAndAssigningSelf()
+usingMutatingAndAssigningSelf.changeName()
+
+
+enum switchStatus {
+    case pause
+    case start
+    case stop
+    
+    mutating func changeStatus() {
+        switch self { // -> self는 swiftchStatus 의미
+        case .pause:
+            self = .start
+            print("start로 변경")
+        case .start:
+            self = .pause
+            print("pause로 변경")
+        case .stop:
+            self = .start
+            print("start로 변경")
+        }
+    }
+}
+//열거형의 변경 메서드는 동일한 열거형에서 다른 케이스로 암시적으로 self 파라미터를 설정할 수 있다.
+
+var nowStatus = switchStatus.stop
+nowStatus.changeStatus()
+nowStatus.changeStatus()
+nowStatus.changeStatus()
+
+ 
+
+
