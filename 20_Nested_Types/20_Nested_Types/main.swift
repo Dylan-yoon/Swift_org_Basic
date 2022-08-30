3b9cda60e7862ec924396ff2ea95f20e56f894c8
dylan-yoon <d.dylany21@gmail.com>
2022-08-30 13:54:53 +0900

    10 임시저장

--- 20_Nested_Types/20_Nested_Types/main.swift
+++ 20_Nested_Types/20_Nested_Types/main.swift
@@ -5,7 +5,63 @@
 //  Created by Dylan_Yoon on 2022/06/20.
 //
 
-import Foundation
+print("Nested Types")
 
-print("Hello, World!")
+//MARK: -Nested Types in Action
+
+
+
+struct BlackjackCard {
+
+    // nested Suit enumeration
+    enum Suit: Character {
+        case spades = "♠", hearts = "♡", diamonds = "♢", clubs = "♣"
+    }
+
+    // nested Rank enumeration
+    enum Rank: Int {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
+        struct Values {
+            let first: Int, second: Int?
+        }
+        var values: Values {
+            switch self {
+            case .ace:
+                return Values(first: 1, second: 11)
+            case .jack, .queen, .king:
+                return Values(first: 10, second: nil)
+            default:
+                return Values(first: self.rawValue, second: nil)
+            }
+        }
+    }
+
+    // BlackjackCard properties and methods
+    let rank: Rank, suit: Suit
+    var description: String {
+        var output = "suit is \(suit.rawValue),"
+        output += " value is \(rank.values.first)"
+        if let second = rank.values.second {
+            output += " or \(second)"
+        }
+        return output
+    }
+}
+
+
+var temp = BlackjackCard(rank: .ace, suit: .clubs)
+print(temp.description)
+temp.suit
+
+//MARK: -Referring to Nested Types
+
+
+
+func aaa() -> Int {
+    let a = 1
+    return a
+}
+
+aaa()
 
