8c40608f8d8345903aaa2c086f97b190407acd0d
dylan-yoon <d.dylany21@gmail.com>
2022-09-02 11:38:36 +0900

    14 setting mark initialization

--- 14_Initalization/14_Initalization/main.swift
+++ 14_Initalization/14_Initalization/main.swift
@@ -4,8 +4,60 @@
 //
 //  Created by Dylan_Yoon on 2022/06/20.
 //
+print("Initialization")
 
-import Foundation
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Setting Initial Values for Stored Properties
+print("Setting Initial Values for Stored Properties")
 
-print("Hello, World!")
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Customizing Initalization
+print("Customizing Initalization")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Default Initializers
+print("Default Initializers")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Initializer Delegation for Value Types
+print("Initializer Delegation for Value Types")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Class Inheritance and Initialization
+print("Class Inheritance and Initialization")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Failable Initializers
+print("Failable Initializers")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Required Initializaers
+print("Required Initializaers")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Setting a Default Property Value with Closure or Function
+print("Setting a Default Property Value with Closure or Function")
+
 
