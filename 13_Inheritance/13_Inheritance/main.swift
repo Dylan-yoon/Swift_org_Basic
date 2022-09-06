a171d7369cf763fe24d9607d6bc15e0e7453a9ab
dylan-yoon <d.dylany21@gmail.com>
2022-09-07 02:40:41 +0900

    13 임시저장

--- 13_Inheritance/13_Inheritance/main.swift
+++ 13_Inheritance/13_Inheritance/main.swift
@@ -5,7 +5,59 @@
 //  Created by Dylan_Yoon on 2022/06/20.
 //
 
-import Foundation
+print("Inheritance")
 
-print("Hello, World!")
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Defining a Base Class
+print("Defining a Base Class")
+
+
+class SuperKAKAO {
+    var company = "KAKAO"
+    var president = "김범수"
+    var stock = 70000
+    var infoStock: String {
+        return "stock info is \(stock)👍🏻"
+    }
+    
+    func what() {}
+}
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -SubClassing
+print("SubClassing")
+
+class SubKAKAOGames: SuperKAKAO {
+    func aa() {
+        print(super.stock)
+    }
+}
+class SubSubKAKAOVEX: SubKAKAOGames {}
+
+class SubKAKAOMobility: SuperKAKAO {
+    
+}
+
+let aaaa = SubKAKAOGames()
+aaaa.aa()
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Overriding
+print("Overriding")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Preventing Overrides
+print("Preventing Overrides")
+
+
+
+
 
