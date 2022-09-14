e72d4d6f743dbccf8e9023446665dfe5aa714d17
Dylan-Yoon <d.dylany21@gmail.com>
2022-09-14 19:09:13 +0900

    08 enum ~연관값

--- 08_Enumerations/08_Enumerations/main.swift
+++ 08_Enumerations/08_Enumerations/main.swift
@@ -11,6 +11,20 @@ print("------------------------------------------------")
 //MARK: -Enumeration Syntax
 print("Enumeration Syntax")
 
+enum SomeEnumeration {
+    //definition
+}
+
+enum PracticeExample1 {
+    case mac
+    case iPhone
+    case macbook
+    case appleWatch
+}
+
+enum PracticeExample2 {
+    case mac, iPhone, macbook, appleWatch
+}
 
 
 print("------------------------------------------------")
@@ -18,6 +32,22 @@ print("------------------------------------------------")
 //MARK: -Matching Enumeration Values with Switch Statement
 print("Matching Enumeration Values with Switch Statement")
 
+enum SwitchWithEnum {
+    case stop
+    case start
+    case pause
+}
+
+var stateButton = SwitchWithEnum.stop
+
+switch stateButton {
+case .stop:
+    print("정지버튼입니다.")
+case .start:
+    print("시작버튼입니다.")
+case .pause:
+    print("정지버튼입니다.")
+}
 
 
 print("------------------------------------------------")
@@ -25,12 +55,46 @@ print("------------------------------------------------")
 //MARK: -Iterating over Enumeration Cases
 print("Iterating over Enumeration Cases")
 
+enum IteratingEnumeration: CaseIterable {
+    case red
+    case yellow
+    case green
+    case blue
+    case black
+    
+}
+let whatIsType = IteratingEnumeration.allCases // [IteratingEnumeration]
+for color in IteratingEnumeration.allCases {
+    print("이번 케이스는 -", color, "-색 입니다.")
+}
+
 
 
 print("------------------------------------------------")
 print("------------------------------------------------")
 //MARK: -Associated Values
 print("Associated Values")
+
+
+
+enum Barcode {
+    case upc(Int, Int, Int, Int)
+    case qrCode(String)
+}
+
+var productBarcode = Barcode.upc(8, 85909, 51226, 3)
+//productBarcode = .qrCode("ABCDEFGHIJKLMNOP")
+
+switch productBarcode {
+case .upc(let numberSystem, let manufacturer, _, let check):
+    print("UPC: \(numberSystem), \(manufacturer), \("product"), \(check).")
+case .qrCode(let productCode):
+    print("QR code: \(productCode).")
+}
+
+
+
+
 
 
 
@@ -53,4 +117,4 @@ print("Initializing from a Raw Value")
 print("------------------------------------------------")
 print("------------------------------------------------")
 //MARK: -Recursive Enumerations)
-print("Recursive Enumerations)")
+print("Recursive Enumerations")
