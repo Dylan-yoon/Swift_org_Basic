2caf5dd2064d34d2671254d2f506d51d625c4b13
Dylan-Yoon <d.dylany21@gmail.com>
2022-09-14 16:22:29 +0900

    08 enum 초기설정

--- 08_Enumerations/08_Enumerations/main.swift
+++ 08_Enumerations/08_Enumerations/main.swift
@@ -4,8 +4,53 @@
 //
 //  Created by Dylan_Yoon on 2022/06/20.
 //
+print("Enumeration")
 
-import Foundation
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Enumeration Syntax
+print("Enumeration Syntax")
 
-print("Hello, World!")
 
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Matching Enumeration Values with Switch Statement
+print("Matching Enumeration Values with Switch Statement")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Iterating over Enumeration Cases
+print("Iterating over Enumeration Cases")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Associated Values
+print("Associated Values")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Raw Values
+print("Raw Values")
+
+
+print("------------------------------------------------")
+//MARK: Implicitly Assigned Raw Values
+print("Implicitly Assigned Raw Values")
+
+print("------------------------------------------------")
+//MARK: Initializing from a Raw Value
+print("Initializing from a Raw Value")
+
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Recursive Enumerations)
+print("Recursive Enumerations)")
