bcf75ee913ace894fe882f718b726f502840c037
Dylan-Yoon <d.dylany21@gmail.com>
2022-10-08 18:27:23 +0900

    12 subscripts 예제 및 swift org

--- 12_Subscripts/12_Subscripts/main.swift
+++ 12_Subscripts/12_Subscripts/main.swift
@@ -5,7 +5,99 @@
 //  Created by Dylan_Yoon on 2022/06/20.
 //
 
-import Foundation
+print("Subscripts")
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Subscript Syntax
+print("Subscript Syntax")
 
-print("Hello, World!")
+struct SnytaxOfSubscripts {
+    subscript(index: Int) -> Int {
+        get {
+            return index
+        }
+    }
+}
+let syntaxOfSubscripts = SnytaxOfSubscripts()
+print(syntaxOfSubscripts[10])
 
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Subscript Usage
+print("Subscript Usage")
+
+var wallet = ["1만원권": 2, "5천원권": 5, "1천원권": 24]
+wallet = ["5만원권": 10]
+//subscripts 에 할당하여 딕셔너리에 값을 설정 할 수 있음
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Subscript Options
+print("Subscript Options")
+
+// swift.org Example
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+//
+//var matrix = Matrix(rows: 2, columns: 2)
+//
+//matrix[0, 1] = 1.5
+//matrix[1, 0] = 3.2
+//
+//let someValue = matrix[2, 2]
+
+//하나의 스트럭트에 여러개의 서브 스크립트
+
+struct Multiply {
+    var times: Int = 10
+    var multy: Int = 2
+    
+    subscript(inputTimes: Int) -> Int {
+        return times * inputTimes
+    }
+    
+//    Invalid redeclaration of 'subscript(_:)'
+//    subscript(inputMulty: Int) -> Int {
+//        return multy * inputMulty
+//    }
+}
+
+var multy = Multiply()
+
+
+print("------------------------------------------------")
+print("------------------------------------------------")
+//MARK: -Type Subscripts
+print("Type Subscripts")
+
+
+enum Planet: Int {
+    case mercury = 1, venus, earth, mars, jupiter, saturn, uranus, neptune
+    static subscript(n: Int) -> Planet {
+        return Planet(rawValue: n)!
+    }
+}
+let mars = Planet[4]
+print(mars)
--- HigherOrderFunction/HigherOrderFunction/main.swift
+++ HigherOrderFunction/HigherOrderFunction/main.swift
@@ -6,76 +6,118 @@
 //
 
 //map
-struct ExampleMap {
-    
-    let transedMapDictionary: [String: Int] = ["one": 10, "two": 19, "three": 32, "four": 40, "five": 35]
-    let transedMapTuple: Set<Int> = [10, 19, 32, 40, 35]
-    
-    //@inlinable public func map<T>(_ transform: (Element) throws -> T) rethrows -> [T]
-    func mapArray() {
-        let transedMapArray: [Int] = [10, 19, 32, 40, 35]
-        let transedMapArrayString: [String] = ["10", "19", "32", "40", "35"]
-        let transedMapArrayAny: [Any] = [10, "32", 313.1, (10, "132")]
-        
-        print("기본!")
-        let new = transedMapArray.map ({ (element: Int) -> Int in
-            return element * 10
-        })
-        print(new)
-        
-        print("문자열로도 가능하다.! -> string 명시를 해주어야하는데 왜 해주어야할까?")
-        let newstring = transedMapArrayString.map { (element: String) -> String in
-            if element == "32" {
-                return "999999"
-            }
-            return element
+//struct ExampleMap {
+//
+//    let transedMapDictionary: [String: Int] = ["one": 10, "two": 19, "three": 32, "four": 40, "five": 35]
+//    let transedMapTuple: Set<Int> = [10, 19, 32, 40, 35]
+//
+//    //@inlinable public func map<T>(_ transform: (Element) throws -> T) rethrows -> [T]
+//    func mapArray() {
+//        let transedMapArray: [Int] = [10, 19, 32, 40, 35]
+//        let transedMapArrayString: [String] = ["10", "19", "32", "40", "35"]
+//        let transedMapArrayAny: [Any] = [10, "32", 313.1, (10, "132")]
+//
+//        print("기본!")
+//        let new = transedMapArray.map ({ (element: Int) -> Int in
+//            return element * 10
+//        })
+//        print(new)
+//
+//        print("문자열로도 가능하다.! -> string 명시를 해주어야하는데 왜 해주어야할까?")
+//        let newstring = transedMapArrayString.map { (element: String) -> String in
+//            if element == "32" {
+//                return "999999"
+//            }
+//            return element
+//        }
+//        print(newstring)
+//
+//        print("다른 타입으로 변경도 가능")
+//        let intToString = transedMapArray.map({ (number: Int) -> String in
+//            return "\(number)"
+//        })
+//
+//        print(intToString)
+//
+//        print("closure 축약1")
+//        let short1 = transedMapArray.map { element -> Int in
+//            return element * 10
+//        }
+//        print(short1)
+//
+//        print("closure 축약2")
+//        let short2 = transedMapArray.map { element in
+//            element * 10
+//        }
+//        print(short2)
+//
+//        print("closure 축약3")
+//        let short3 = transedMapArray.map { $0 * 10 }
+//        print(short3)
+//
+//        print("Any 타입")
+//        let anytype = transedMapArrayAny.map { element -> Any in
+//            if element is Int {
+//                return 10000
+//            }
+//
+//            return element
+//        }
+//        print(anytype)
+//    }
+//}
+//
+//var a = ExampleMap()
+//a.mapArray()
+//
+//struct ExampleCompactMap {
+//    func aa() {
+//        let transedMapArray: [Int] = [10, 19, 32, 40, 35]
+//
+//        let a = transedMapArray.flatMap(<#T##transform: (Int) throws -> Sequence##(Int) throws -> Sequence#>)
+//
+//        print(a)
+//    }
+//}
+
+
+struct TimesTable {
+    var multiplier: Int
+    subscript(index: Int) -> Int {
+        get {
+            return multiplier * index
         }
-        print(newstring)
-        
-        print("다른 타입으로 변경도 가능")
-        let intToString = transedMapArray.map({ (number: Int) -> String in
-            return "\(number)"
-        })
-        
-        print(intToString)
-        
-        print("closure 축약1")
-        let short1 = transedMapArray.map { element -> Int in
-            return element * 10
+        set {
+            print(newValue)
         }
-        print(short1)
-        
-        print("closure 축약2")
-        let short2 = transedMapArray.map { element in
-            element * 10
-        }
-        print(short2)
-        
-        print("closure 축약3")
-        let short3 = transedMapArray.map { $0 * 10 }
-        print(short3)
-        
-        print("Any 타입")
-        let anytype = transedMapArrayAny.map { element -> Any in
-            if element is Int {
-                return 10000
-            }
-            
-            return element
-        }
-        print(anytype)
     }
 }
+var threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+// Prints "six times three is 18"
 
-var a = ExampleMap()
-a.mapArray()
+let five = threeTimesTable[5]
+let six = threeTimesTable[6]
+threeTimesTable.multiplier = 10
+print(threeTimesTable[10])
 
-struct ExampleCompactMap {
-    func aa() {
-        let transedMapArray: [Int] = [10, 19, 32, 40, 35]
-        
-        let a = transedMapArray.flatMap(<#T##transform: (Int) throws -> Sequence##(Int) throws -> Sequence#>)
-        
-        print(a)
+//print(five)
+//threeTimesTable.multiplier = 10
+//print(threeTimesTable)
+
+print("-------------------")
+var first: String = "dylan"
+var second: String = "yoon"
+var name: String {
+    get {
+        return first + " " + second
+    }
+    set {
+        print(newValue, "newvalue")
     }
 }
+print(name)
+second = "kim"
+name = "string"
+first = "jun"
+print(name)
